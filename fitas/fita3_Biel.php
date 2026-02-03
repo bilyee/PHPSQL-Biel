cb98f1c954a5460e7b5e4f2cacd8a0b3b503d562
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-02-03 15:49:26 +0100

    Update fita3_Biel.php

--- fitas/fita3_Biel.php
+++ fitas/fita3_Biel.php
@@ -1,7 +1,4 @@
 <?php
-    //connexió dins block try-catch:
-    //  prova d'executar el contingut del try
-    //  si falla executa el catch
     try {
         $hostname = "localhost";
         $dbname = "classicmodels";
@@ -9,24 +6,22 @@
         $pw = "Bat_3009";
         $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
     } catch (PDOException $e) {
-        // obtenim missatge d'error de l'excepció llançada
         echo "Error connectant a la BD: " . $e->getMessage() . "<br>\n";
         exit;
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Aqui debemos listar los clientes que coincidan o tengan una coincidencia parcial con el filtro
         $customerName = $_POST["customerName"];
-        $stmt = $pdo->prepare("SELECT customerName, contactFirstName, contactLastName FROM customers WHERE customerName LIKE ?");
-        $stmt->execute(["%$customerName%"]);
+        $stmt = $pdo->prepare("SELECT customerName, contactFirstName, contactLastName, city FROM customers WHERE customerName LIKE ? OR contactFirstName LIKE ? OR contactLastName LIKE ?");
+        $stmt->execute(["%$customerName%", "%$customerName%", "%$customerName%"]);
         echo "<h3>Resultats per el filtre: '$customerName'</h3>";
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<p>{$row['customerName']} - {$row['contactFirstName']} {$row['contactLastName']}</p>";
+            echo "<p>{$row['customerName']} - {$row['contactFirstName']} {$row['contactLastName']} ({$row['city']})</p>";
         }
     }
 
     try {
-        $countries = $pdo->query("SELECT Code, Name FROM country ORDER BY Name");
+        $customers = $pdo->query("SELECT customerName, contactFirstName, contactLastName FROM customers");
     } catch (PDOException $e) {
         echo "Error " . $e->getMessage();
         exit;
@@ -40,7 +35,6 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- Hay que hacer un formulario que filtre por clientes (customers) a partir del nombre del cliente, nombre o apellido del contacto (mirar los 3 campos) -->
     <h2>Filtres per una sola taula</h2>
     <form action="" method="POST">
         <label for="customerName">Nom del client:</label>
